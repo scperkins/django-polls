e049a978ffd0790ee0e5d04b03d7fb2749944fac
Sean Perkins <perkinsean@gmail.com>
2015-01-25 18:21:01 -0500

    add basic 404 detail

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 from django.template import RequestContext, loader
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from polls.models import Question
 # Create your views here.
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
-
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+    
 def detail(request, question_id):
-    return HttpResponse("You're looking at question {0}.".format(question_id))
+    try:
+        question = Question.objects.get(pk=question_id)
+    except:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of the question %s."
