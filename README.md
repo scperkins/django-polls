c686b78de38325967c826aef3fcb0af02b6f6fae
Sean Perkins <perkinsean@gmail.com>
2015-01-25 12:28:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+django-polls
+===
+
+Getting Started
+---
+
+### Setup ###
+1. Make a python virtualenv in your chosen directory: ``virtualenv env``
+2. Activate the virtual environment: ``source env/bin/activate``
+3. Install Django from pip: ``pip install Django``
+4. Optional: edit ``TIMEZONE`` in ``manage.py`` to match your timezone
+4. Run the server ``python manage.py runserver``
+5. Check ``localhost:8000`` 
+6. Proceed with the tutorial
---
+++ polls/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from polls.models import Question, Choice
+# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (None,               {'fields': ['question_text']}),
+            ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Choice)
