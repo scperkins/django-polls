2f02ca6afe70e8f657b8d1a49f41fbc63989fa81
Sean Perkins <perkinsean@gmail.com>
2015-01-27 15:03:33 -0500

    finished part 4 of the tutorial

--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import get_object_or_404, render
-from django.template import RequestContext, loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
-
+from django.views import generic
 from polls.models import Question, Choice
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-    
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
@@ -36,4 +36,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(p.id)))
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
