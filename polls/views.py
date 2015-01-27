1ac73f59edef5cf5b44953b385bd1a47aa44de7e
Sean Perkins <perkinsean@gmail.com>
2015-01-27 11:52:59 -0500

    added results view

--- polls/views.py
+++ polls/views.py
@@ -17,17 +17,17 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of the question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice - p.choice_set.get(pk=request.POST['choice'])
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question in voting form
         return render(request, 'polls/detail.html', {
-            'question':p,
+            'question': p,
             'error_message': "You didn't select a choice.",
         })
     else:
