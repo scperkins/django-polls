c2c177f006e602799df63b8e2538d13f0a2f12ba
Sean Perkins <perkinsean@gmail.com>
2015-01-30 17:20:37 -0500

    added getquery and tests for results view

--- polls/views.py
+++ polls/views.py
@@ -25,6 +25,9 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 def vote(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
     try:
