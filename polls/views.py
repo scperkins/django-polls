be68903d672c7bc89a640b57c98eb05d5d60b9f9
Sean Perkins <perkinsean@gmail.com>
2015-01-31 14:42:08 -0500

    something

--- polls/views.py
+++ polls/views.py
@@ -24,6 +24,9 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+    
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 def vote(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
